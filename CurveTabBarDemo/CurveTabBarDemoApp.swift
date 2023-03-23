5fdbbd735dcbfd234a1c602a8ba9fc1500f56125
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-03-23 07:14:14 +0600

    initialcommit

--- CurveTabBarDemo/CurveTabBarDemoApp.swift
+++ CurveTabBarDemo/CurveTabBarDemoApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CurveTabBarDemoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TestReorderTabItemsPerSelection()
         }
     }
 }
