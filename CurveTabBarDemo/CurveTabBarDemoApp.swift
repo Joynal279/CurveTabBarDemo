7fc772ad6fa046d24a423fbc67a562810d46d8bd
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-03-23 07:07:03 +0600

    Initial Commit

---
+++ CurveTabBarDemo/CurveTabBarDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurveTabBarDemoApp.swift
+//  CurveTabBarDemo
+//
+//  Created by Joynal Abedin on 23/3/23.
+//
+
+import SwiftUI
+
+@main
+struct CurveTabBarDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
